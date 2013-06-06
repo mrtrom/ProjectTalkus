ac6857ef1bb92690ec8ab4e41c2f78dee4290501
Alexander Ochoa <a.ochoa.s@outlook.com>
2013-06-06 12:01:51 -0400

    version working with chat

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -15,9 +15,7 @@ var App = angular.module('myApp', [
 
 App.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {    
     $routeProvider.when('/', {templateUrl: 'partials/index',controller: 'LoginController'});
-    $routeProvider.when('/chat/', {templateUrl: 'partials/chat',controller: 'ChatesController'});
-    $routeProvider.when('/chates/', {templateUrl: 'partials/chates',controller: 'ChatesController'});
-    $routeProvider.when('/account/:username', {templateUrl: 'partials/profile',controller: 'AccountController'});
+    $routeProvider.when('/chat/', {templateUrl: 'partials/chat',controller: 'AccountController'});
     $routeProvider.otherwise({redirectTo: '/'});
   }
 ]);
--- client/public/js/controllers/chates.js
+++
@@ -1,6 +0,0 @@
-Modules.controllers.controller('ChatesController', ['$routeParams', '$rootScope', '$scope', '$http', '$location',  'Session', 'User',
-    function($routeParams, $rootScope, $scope, $http, $location, Session, User) {
-    
-    
-    
-}]);
