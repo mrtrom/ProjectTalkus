93cd408924ae03766cedaae7516780e1b567243f
srobledo <sergiorobledo2k5@gmail.com>
2014-03-11 19:11:04 -0500

    update chat

--- app/scripts/personalScripts/chat.js
+++ app/scripts/personalScripts/chat.js
@@ -4,7 +4,10 @@ $(function(){
 		$("#sidebar-wrapper-anonym").toggleClass("active");
 		$(".view.ng-scope").toggleClass("slide-left");
 	});
-
+  $('#anonym-profile').click(function(){
+    $('#anonym-popup').toggle();
+    $('#popoverInfoMeModal').css('display','none');
+  });
 	$("#menu-close-anonym").click(function(e) {
 		e.preventDefault();
 		$("#sidebar-wrapper-anonym").toggleClass("active");
@@ -12,9 +15,11 @@ $(function(){
 	});
   $('#menu-close-anonym, #conversation , .navbar .container , .wraper-chat').click(function(){
     $('#popoverInfoMeModal').css('display','none');
+    $('#anonym-popup').css('display','none');
   });
   $('#locationapi').geocomplete();
 	$('#popoverInfoMe').on('click', function(){
 		$('#popoverInfoMeModal').toggle();
+    $('#anonym-popup').css('display','none');
 	});
 });
