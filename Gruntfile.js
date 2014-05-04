57e2e7db2a59f2e60026563cf856046aa4038fcd
mrtrom <ochoaseguralex@gmail.com>
2014-05-04 17:18:11 -0500

    production env working

--- Gruntfile.js
+++ Gruntfile.js
@@ -214,7 +214,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '**/*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/public/images'
         }]
       }
